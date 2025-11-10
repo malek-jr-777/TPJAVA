51b45893080aa3b95ffb3a192722c48a10a72192
malek-jr-777 <malekmabrouk082@gmail.com>
2025-11-10 10:53:27 +0100

    Add files via upload

--- Rectangle.java
+++ Rectangle.java
@@ -1,19 +1,26 @@
-package tp2crcl;
-
-
+package TP4;
 public class Rectangle {
-	double a;
-	double b;
-	
-public Rectangle(double z, double y){
-		 a=z;
-		 b=y;
-}
-	public double surfcrec() {
-		return a*b;
-}
 
+    Double longueur;
+    Double largeur;
+
+    public Rectangle(Double longueur, Double largeur) {
+        this.longueur = longueur;
+        this.largeur = largeur;
+    }
+
+    public Double surface() {
+        return longueur * largeur;
+    }
+
+    public Double perimetre() {
+        return 2 * (longueur + largeur);
+    }
+
+    public void afficher() {
+        System.out.println("longueur = " + longueur);
+        System.out.println("largeur  = " + largeur);
+        System.out.println("surface  = " + surface());
+        System.out.println("perimetre = " + perimetre());
+    }
 }
-	
-
-
---
+++ TestRectangle.java
@@ -0,0 +1,8 @@
+package TP4;
+
+public class TestRectangle {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(5.0, 3.0);
+        r1.afficher();
+    }
+}
